d1f58014fcb3d3add4ccc444fc9b60213db97cce
Pei <jzha471@aucklanduni.ac.nz>
2017-11-05 14:16:11 +1300

    13. Faceted Builder

---
+++ Design Patterns/Builder/Faceted Builder/Faceted Builder/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faceted_Builder
+{
+    public class Person
+    {
+        //address
+        public string Street { get; set; }
+
+        public string Postcode { get; set; }
+        public string City { get; set; }
+        public string StreetAddress { get; set; }
+
+        public string CompanyName { get; set; }
+        public string Position { get; set; }
+        public int AnnualIncome { get; set; }
+
+        public override string ToString()
+        {
+            return
+                $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder //facade
+    {
+        //reference!
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+
+    }
+
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Lives.At("123 London Road")
+                      .In("London")
+                      .WithPostcode("SW12AC")
+                .Works
+                    .At("Fabrikam")
+                    .AsA("Engineering")
+                    .Earning(123000);
+
+            Console.WriteLine(person);
+        }
+    }
+}
